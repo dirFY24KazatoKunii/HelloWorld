ce4f73c38810dd981316f4011958770def9245ee
dirFY24KazatoKunii <kazato.kunii.n323@dirbato.co.jp>
2024-04-23 09:13:08 +0900

    最初のコミット

---
+++ HelloWorld/src/first/date.java
@@ -0,0 +1,16 @@
+package first;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Period;
+
+public class date {
+
+    public static void main(String[] args) {
+        System.out.println(Duration.between(LocalTime.of(9, 0), LocalTime.of(2, 0)));
+
+
+    }
+
+}
